d50b60e94b286a86881e7a6abb7344ae6e370dff
Saravana <godsaran05@gmail.com>
2025-09-21 18:46:36 +0530

    origin allowed for all

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
