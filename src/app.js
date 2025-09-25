3a76eddc9434eb7b45608af3ce00be95ffc77d0f
Saravana <godsaran05@gmail.com>
2025-09-25 14:45:02 +0530

    api changed

--- src/app.js
+++ src/app.js
@@ -29,10 +29,10 @@ const requestRouter = require("./routes/request");
 
 const userRouter = require("./routes/user");
 
-app.use("/auth", authRouter);
-app.use("/profile", profileRouter);
-app.use("/request", requestRouter);
-app.use("/user", userRouter);
+app.use("api/auth", authRouter);
+app.use("api/profile", profileRouter);
+app.use("api/request", requestRouter);
+app.use("api/user", userRouter);
 
 connectDB()
   .then(() => {
