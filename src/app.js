1e39f63d8f1ba5ac43ffcd82fd43ba6456bf2559
Saravana <godsaran05@gmail.com>
2025-12-12 15:52:06 +0530

    changedd running port

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 
 app.use(
   cors({
-    origin: "http://localhost:5174",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
