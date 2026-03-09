97db09656558c8b277305338e10c3404d5bb8044
Saravana <godsaran05@gmail.com>
2026-03-09 14:57:26 +0530

    enabled redis

--- src/app.js
+++ src/app.js
@@ -71,7 +71,7 @@ initializeSocket(server);
 //   .catch((err) => console.log(err));
 
 const startServer = async () => {
- // await connectRedis(); // Ensure Redis is connected before server starts
+  await connectRedis(); // Ensure Redis is connected before server starts
   await connectDB();
   const server = app.listen(process.env.PORT, () => {
     console.log("Server running on port "+process.env.PORT);
